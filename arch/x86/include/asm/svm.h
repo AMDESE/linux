547b97a76fbca8347ad7166f91b4bc234a403ec3
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2024-05-31 11:14:14 +0000

    x86: KVM: SVM: Increase X2AVIC limit upto 4096 vcpus
    Newer AMD platforms enhance x2AVIC feature to support upto 4096 vcpus.
    This capatility is detected via CPUID_Fn8000000A_ECX[x2AVIC_EXT].
    
    Modify the SVM driver to check the capability. If detected, extend bitmask
    for guest max physical APIC ID to 0xFFF, increase maximum vcpu index to
    4095, and increase the size of the Phyical APIC ID table from 4K to 32K in
    order to accomodate upto 4096 entries.
    
    Please see the AMD64 Architecture Programmer’s Manual Volume 2:
    System Programming for more detail
    
    Note: Also include commit 3ec7a1b2743c ("KVM: SVM: Fix a benign off-by-one bug in AVIC physical table mask")
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>

--- arch/x86/include/asm/svm.h
+++ arch/x86/include/asm/svm.h
@@ -180,19 +180,25 @@ enum avic_ipi_failure_cause {
 	AVIC_IPI_FAILURE_INVALID_IPI_VECTOR,
 };
 
-#define AVIC_PHYSICAL_MAX_INDEX_MASK	GENMASK_ULL(9, 0)
+#define AVIC_PHYSICAL_MAX_INDEX_MASK	GENMASK_ULL(8, 0)
+#define AVIC_PHYSICAL_MAX_INDEX_4K_MASK	GENMASK_ULL(11, 0)
 
 /*
- * For AVIC, the max index allowed for physical APIC ID
- * table is 0xff (255).
+ * For AVIC, the max index allowed for physical APIC ID table is 0xfe (254), as
+ * 0xff is a broadcast to all CPUs, i.e. can't be targeted individually.
  */
 #define AVIC_MAX_PHYSICAL_ID		0XFEULL
 
 /*
- * For x2AVIC, the max index allowed for physical APIC ID
- * table is 0x1ff (511).
+ * For x2AVIC, the max index allowed for physical APIC ID table is 0x1ff (511).
+ * For x2AVIC (dense), the max index allowed for physical APIC ID table is 0xfff (4095).
  */
 #define X2AVIC_MAX_PHYSICAL_ID		0x1FFUL
+#define X2AVIC_MAX_PHYSICAL_ID_4K	0xFFFUL
+
+static_assert((AVIC_MAX_PHYSICAL_ID & AVIC_PHYSICAL_MAX_INDEX_MASK) == AVIC_MAX_PHYSICAL_ID);
+static_assert((X2AVIC_MAX_PHYSICAL_ID & AVIC_PHYSICAL_MAX_INDEX_MASK) == X2AVIC_MAX_PHYSICAL_ID);
+static_assert((X2AVIC_MAX_PHYSICAL_ID_4K & AVIC_PHYSICAL_MAX_INDEX_4K_MASK) == X2AVIC_MAX_PHYSICAL_ID_4K);
 
 #define AVIC_HPA_MASK	~((0xFFFULL << 52) | 0xFFF)
 #define VMCB_AVIC_APIC_BAR_MASK		0xFFFFFFFFFF000ULL
