854c6ceddf46057138d0ff6dfb9e000dbb90e88f
Elliot Berman <quic_eberman@quicinc.com>
2024-11-22 09:29:38 -0800

    mm/filemap: Pass address_space mapping to ->free_folio()
    The plan is to be able to support multiple allocators for guest_memfd
    folios. To allow each allocator to handle release of a folio from a
    guest_memfd filemap, ->free_folio() needs to retrieve allocator
    information that is stored on the guest_memfd inode.
    
    ->free_folio() shouldn't assume that folio->mapping is set/valid, and
    the mapping is well-known to callers of .free_folio(). Hence, pass
    address_space mapping to ->free_folio() for the callback to retrieve
    any necessary information.
    
    Link: https://lore.kernel.org/all/15f665b4-2d33-41ca-ac50-fafe24ade32f@redhat.com/
    Suggested-by: David Hildenbrand <david@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>
    Tested-by: Mike Day <michael.day@amd.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    (cherry picked from commit 4a24f13a158541b5425093899f84e30fc1e31a50)
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -786,7 +786,7 @@ static int __remove_mapping(struct address_space *mapping, struct folio *folio,
 		swap_cluster_unlock_irq(ci);
 		put_swap_folio(folio, swap);
 	} else {
-		void (*free_folio)(struct folio *);
+		void (*free_folio)(struct address_space *, struct folio *);
 
 		free_folio = mapping->a_ops->free_folio;
 		/*
@@ -815,7 +815,7 @@ static int __remove_mapping(struct address_space *mapping, struct folio *folio,
 		spin_unlock(&mapping->host->i_lock);
 
 		if (free_folio)
-			free_folio(folio);
+			free_folio(mapping, folio);
 	}
 
 	return 1;
