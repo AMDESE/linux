72daef4db2791b46869b4c6350b819c4163f9643
Ackerley Tng <ackerleytng@google.com>
2025-04-24 16:59:21 -0700

    KVM: guest_memfd: Support guestmem_hugetlb as custom allocator
    This patch adds support for guestmem_hugetlb as the first custom
    allocator in guest_memfd.
    
    If requested at guest_memfd creation time, the custom allocator will
    be used in initialization and cleanup.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -738,6 +738,9 @@ static inline u64 kvm_gmem_get_supported_flags(struct kvm *kvm)
 	if (!kvm || kvm_arch_supports_gmem_init_shared(kvm))
 		flags |= GUEST_MEMFD_FLAG_INIT_SHARED;
 
+	if (IS_ENABLED(CONFIG_KVM_GMEM_HUGETLB))
+		flags |= GUEST_MEMFD_FLAG_HUGETLB;
+
 	return flags;
 }
 #endif
