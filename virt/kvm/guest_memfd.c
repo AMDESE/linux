fadfd950e74fe144c39ff8e8188e812a6c77fa4f
Alexey Kardashevskiy <aik@amd.com>
2025-10-09 13:35:39 +1100

    KVM: guest_memfd: Fix slab-allocated inodes
    Normally kvm_gmem_super_operations::alloc_inode() hooks call
    alloc_inode_sb() so vfs_node is initialised and then set up the rest.
    If not set up, then garbage remains in various fields.
    
    The immediate effect of this garbage is info->allocator_ops!=NULL
    when running with 4K pages (==no custom hugetlb allocator) and this
    trigger unwanted behaviour leading to crashes.
    
    But passing __GFP_ZERO to alloc_inode_sb() brakes it as
    file_inode(file)->i_mapping->i_private_list is uninitialised at
    the end of __kvm_gmem_create() so list_add() crashes.
    
    Do proper setup of all fields in the kvm_gmem_alloc_inode() hook.
    Use the opportunity to move mt_init() to where it belongs.
    
    Signed-off-by: Alexey Kardashevskiy <aik@amd.com>

--- virt/kvm/guest_memfd.c
+++ virt/kvm/guest_memfd.c
@@ -1724,6 +1724,11 @@ static struct inode *kvm_gmem_alloc_inode(struct super_block *sb)
 		return NULL;
 
 	mpol_shared_policy_init(&info->policy, NULL);
+	mt_init(&info->shareability);
+#ifdef CONFIG_KVM_GMEM_HUGETLB
+	info->allocator_ops = NULL;
+	info->allocator_private = NULL;
+#endif
 
 	return &info->vfs_inode;
 }
@@ -1946,8 +1951,6 @@ static struct inode *kvm_gmem_inode_create(const char *name, loff_t size,
 	info = KVM_GMEM_I(inode);
 	if (WARN_ON_ONCE(!info))
 		goto out;
-
-	mt_init(&info->shareability);
 
 	err = kvm_gmem_shareability_setup(info, size, flags);
 	if (err)
