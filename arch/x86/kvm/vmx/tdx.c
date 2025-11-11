f14382802c5afc536891a520f251060cb8258b63
Michael Roth <michael.roth@amd.com>
2025-11-11 10:19:16 -0600

    KVM: TDX: Document alignment requirements for KVM_TDX_INIT_MEM_REGION
    Since it was never possible to use a non-PAGE_SIZE-aligned @source_addr,
    go ahead and document this as a requirement, and add a KVM_BUG_ON() in
    the post-populate callback handler to ensure future reworks to
    guest_memfd do not violate this constraint.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -3127,6 +3127,9 @@ static int tdx_gmem_post_populate(struct kvm *kvm, gfn_t gfn, kvm_pfn_t pfn,
 	if (KVM_BUG_ON(kvm_tdx->page_add_src, kvm))
 		return -EIO;
 
+	if (KVM_BUG_ON(!PAGE_ALIGNED(src), kvm))
+		return -EINVAL;
+
 	/*
 	 * Get the source page if it has been faulted in. Return failure if the
 	 * source page has been swapped out or unmapped in primary memory.
