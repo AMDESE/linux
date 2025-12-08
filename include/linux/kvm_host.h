726966cbada5f60e48da016bea391822951a8e39
Michael Roth <michael.roth@amd.com>
2025-12-07 22:30:02 -0600

    KVM: Add support for issuing callbacks once KVM instance is destroyed
    In some cases a task might want to defer exit until the KVM instance has
    reached its final stages before freeing itself. This interface allows
    for callbacks to be queued up for processing at this stage. It is
    expected, in particular, that KVM will no longer hold references to any
    memory that was being used to back the VM prior to shutdown/destroy.
    
    This will be used initially to enable reclaiming of hugepages by
    guest_memfd memory backends, which may rely on refcounts being
    fully-dropped in order to rebuild/return split hugetlb pages to the
    hugetlb pool.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -769,6 +769,14 @@ struct kvm_memslots {
 	int node_idx;
 };
 
+typedef void (*kvm_finalize_workfn)(void *data);
+
+struct kvm_finalize_work {
+	struct list_head list;
+	void *data;
+	kvm_finalize_workfn fn;
+};
+
 struct kvm {
 #ifdef KVM_HAVE_MMU_RWLOCK
 	rwlock_t mmu_lock;
@@ -879,6 +887,7 @@ struct kvm {
 	struct xarray mem_attr_array;
 #endif
 	char stats_id[KVM_STATS_NAME_SIZE];
+	struct list_head finalize_work_list;  /* Work functions needed to finalize VM */
 };
 
 #define kvm_err(fmt, ...) \
@@ -2632,5 +2641,14 @@ void kvm_disable_virtualization(void);
 static inline int kvm_enable_virtualization(void) { return 0; }
 static inline void kvm_disable_virtualization(void) { }
 #endif
+
+/*
+ * In some cases a task might want to defer exit until the KVM instance has
+ * reached its final stages before freeing itself. This interface allows
+ * for callbacks to be queued up for processing at this stage. It is expected,
+ * in particular, that KVM will no longer hold references to any memory that
+ * was being used to back the VM prior to shutdown/destroy.
+ */
+void kvm_enqueue_finalize_work(struct kvm *kvm, kvm_finalize_workfn fn, void *data);
 
 #endif
